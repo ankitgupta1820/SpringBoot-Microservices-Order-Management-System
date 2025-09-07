44f15e1e13ca0eb3355f37a432e4bffffd00bcb3
ankitgupta1820 <ankitgupta20082002.com>
2025-09-08 02:58:40 +0530

    adding integration Test

--- src/test/java/com/ankit/microservice/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/ankit/microservice/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,64 @@
 package com.ankit.microservice.product_service;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.shaded.org.hamcrest.Matchers;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+
+
+
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI="http://localhost";
+		RestAssured.port = port;
+
+
+	}
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				    "name":"iPhone 16",
+				    "description":"this is iphone of apple",
+				    "price":1000
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", notNullValue())
+				.body("name", equalTo("iPhone 16"))
+				.body("description", equalTo("this is iphone of apple"))
+				.body("price", equalTo(1000));
 	}
 
 }
